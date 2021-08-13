a421ec952adbe8ff98049b190fb439afd577100a
mukeshnaagar <mukeshnaagarr@gmail.com>
2021-08-13 16:38:20 +0530

    Updates

---
+++ precache-manifest.3ca2dd140d02a6f175ee9b63983e48ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60169a6b0f6769294bc749e74cdc6544",
+    "url": "/Dogcare/index.html"
+  },
+  {
+    "revision": "cbe3d70dd9f2ff4484b0",
+    "url": "/Dogcare/static/css/main.266173d9.chunk.css"
+  },
+  {
+    "revision": "957da3bcc594992a47de",
+    "url": "/Dogcare/static/js/2.58185527.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Dogcare/static/js/2.58185527.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbe3d70dd9f2ff4484b0",
+    "url": "/Dogcare/static/js/main.74bc0eb3.chunk.js"
+  },
+  {
+    "revision": "da20180b007ace67b2d2",
+    "url": "/Dogcare/static/js/runtime-main.6ae9b1b5.js"
+  }
+]);
